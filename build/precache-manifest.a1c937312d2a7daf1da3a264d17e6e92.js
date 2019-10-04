f60dbc5fd7a33ad062ce43539b82cb5e65d81f4d
Talha_HERE <tshahab@crovateoffshore.com>
2019-10-04 16:05:08 +0500

    first Commit

---
+++ build/precache-manifest.a1c937312d2a7daf1da3a264d17e6e92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "714191e1b86af74f9c54d8c173a53fa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de4b2a89b3635df9170c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "97d876eb0b5a0a1346a5",
+    "url": "/static/js/2.4fccd173.chunk.js"
+  },
+  {
+    "revision": "de4b2a89b3635df9170c",
+    "url": "/static/js/main.8bdfd10e.chunk.js"
+  },
+  {
+    "revision": "0c1400ec95fcc8fa04a7",
+    "url": "/static/js/runtime~main.5134c318.js"
+  }
+]);
